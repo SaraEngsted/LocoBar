6074a2e867e4f434ad1d9edb08fbc2e22e92a88d
naom0649 <naom0649@stud.kea.dk>
2023-08-10 12:44:29 +0200

    ændret til de nye navgivninger

--- app/src/main/java/com/example/locobar/Frontpage.java
+++ app/src/main/java/com/example/locobar/Frontpage.java
@@ -17,24 +17,24 @@ public class Frontpage extends AppCompatActivity {
         setContentView(R.layout.activity_frontpage);
 
         button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(view -> openActivity2());
+        button.setOnClickListener(view -> openMenu());
 
         button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(view -> openActivity3());
+        button2.setOnClickListener(view -> openEditMenu());
     }
 
-    public void openActivity2(){
+    public void openMenu(){
         Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
     }
 
-    public void openActivity3() {
+    public void openEditMenu() {
         Intent intent2 = new Intent(this, EditMenu.class);
         startActivity(intent2);
     }
 
-    public void openActivity4(View view){
-        Intent intent3 = new Intent(Menu.this, Basket.class);
+    public void openBasket(View view){
+        Intent intent3 = new Intent(Frontpage.this, Basket.class);
         startActivity(intent3);
     }
 }
--- app/src/main/java/com/example/locobar/model/CartItem.java
+++ app/src/main/java/com/example/locobar/model/CartItem.java
@@ -1,4 +1,59 @@
 package com.example.locobar.model;
 
+import android.widget.ImageView;
+
 public class CartItem {
+
+    private  String productName;
+    private double price;
+    private int quantity;
+    private ImageView imageView;
+
+
+
+    public CartItem(String productName,double price, int quantity){
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public CartItem(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public CartItem() {
+
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", imageView=" + imageView +
+                '}';
+    }
 }
